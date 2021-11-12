f8bae6c0dc643ca64cc973214239d70b59ca4f1d
NDeleo94 <n.deleo94@gmail.com>
2021-11-12 19:06:52 -0300

    Se definio la entidad Score

---
+++ Salvo/Salvo/Migrations/20211112220339_modifyScoreEntity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Salvo.Migrations
+{
+    public partial class modifyScoreEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Finish",
+                table: "Scores",
+                newName: "FinishDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FinishDate",
+                table: "Scores",
+                newName: "Finish");
+        }
+    }
+}
